0ea4b63ab0e69a00d187a0e65045a5291e21cd07
hrudesh <hrudesh.lenka@gmail.com>
2026-06-19 21:04:55 +0530

    updated the dormant  external endpoint for quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import {useState, useEffect} from 'react';
-import { Card, Button } from '@material-ui/core';
-import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
-import TwitterIcon from '@material-ui/icons/Twitter';
+import { Card, Button } from '@mui/material';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import TwitterIcon from '@mui/icons-material/Twitter';
 import COLOR_ARRAY from './color'
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
     async function fetchData() {
         let randomInteger = Math.floor(COLOR_ARRAY.length * Math.random())
         setBgColor(COLOR_ARRAY[randomInteger])
-        const res = await fetch("https://api.quotable.io/random");
+        const res = await fetch("https://dummyjson.com/quotes/random");
         res
           .json()
           .then(res => setResponseData(res))
@@ -29,10 +29,10 @@ function App() {
   return (
       <div className='body' style={{backgroundColor: bgColor}}>
         <Card id="quote-box">
-            <p id="text" style={{color: bgColor}}><FormatQuoteIcon />{responseData.content}</p>
+            <p id="text" style={{color: bgColor}}><FormatQuoteIcon />{responseData.quote}</p>
             <p id="author" style={{color: bgColor}}>-{responseData.author}</p>
             <div className='button-twitter'>
-              <a id="tweet-quote" href={encodeURI(`http://twitter.com/intent/tweet?text=${responseData.content} -${responseData.author}`)}><TwitterIcon style={{backgroundColor: bgColor, color: 'white'}} /></a>
+              <a id="tweet-quote" href={encodeURI(`http://twitter.com/intent/tweet?text=${responseData.quote} -${responseData.author}`)}><TwitterIcon style={{backgroundColor: bgColor, color: 'white'}} /></a>
               <Button id="new-quote" style={{backgroundColor: bgColor}} onClick={() => fetchData()}>
                 New quote
               </Button>
